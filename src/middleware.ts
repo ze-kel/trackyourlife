49e6590140a6f917fdcf2e032ede1ed7ef12abdc
Kirill Kleymenov <k.kelolov@gmail.com>
2023-01-21 17:10:08 +0400

    fix: middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,11 +3,16 @@ import { withAuth } from "next-auth/middleware";
 export default withAuth({
   callbacks: {
     authorized({ req, token }) {
-      // `/admin` requires admin role
-      if (req.nextUrl.pathname === "/login") {
+      console.log("req.nextUrl.pathname", req.nextUrl.pathname);
+      // There is no need to check for authorization on API routes. TRPC handles that.
+      if (req.nextUrl.pathname.startsWith("/api")) {
         return true;
       }
-      // `/me` only requires the user to be logged in
+
+      // Login page shoud also be acessible to unauthorized users
+      if (req.nextUrl.pathname.startsWith("/login")) {
+        return true;
+      }
       return !!token;
     },
   },
