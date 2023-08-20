db5a59e677b75a3b03589d11512997f2c4b0f263
Kirill Kleymenov <k.kelolov@gmail.com>
2023-08-21 00:08:09 +0300

    feat: reset db

--- prisma/migrations/20230209174140_add_user_settings/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Trackable" ALTER COLUMN "settings" SET DEFAULT '{}';
-
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "settings" JSONB NOT NULL DEFAULT '{}';
--- prisma/migrations/20230402171133_datetime_to_date/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "TrackableRecord" ALTER COLUMN "date" SET DATA TYPE DATE;
