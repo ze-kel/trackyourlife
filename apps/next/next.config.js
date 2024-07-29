60e03c6006cab82bb5fb1a06ed697cf4e93be34e
Kirill Kleymenov <k.kelolov@gmail.com>
2024-07-29 23:11:43 +0300

    config and linter fix

--- apps/next/next.config.js
+++ apps/next/next.config.js
@@ -1,10 +1,3 @@
-import { fileURLToPath } from "url";
-
-//import createJiti from "jiti";
-
-// Import env files to validate at build time. Use jiti so we can load .ts files in here.
-//createJiti(fileURLToPath(import.meta.url))("./src/env");
-
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
@@ -18,11 +11,11 @@ const config = {
 
   /** Enables hot reloading for local packages without a build step */
   transpilePackages: [
-    "@acme/api",
-    "@acme/auth",
-    "@acme/db",
-    "@acme/ui",
-    "@acme/validators",
+    "@tyl/api",
+    "@tyl/auth",
+    "@tyl/db",
+    "@tyl/ui",
+    "@tyl/validators",
   ],
 
   /** We already do linting and typechecking as separate tasks in CI */
