6ff605940bc7c36bce33118b23c8c299bafaad62
Kirill Kleymenov <k.kelolov@gmail.com>
2024-11-29 22:48:44 +0300

    bump packages

--- packages/helpers/src/timezone.ts
+++ packages/helpers/src/timezone.ts
@@ -1,14 +1,10 @@
 import type { TimeZone } from "timezones-list";
-import { toZonedTime } from "date-fns-tz";
+import { TZDate } from "@date-fns/tz";
 
-export const getNowInTimezone = (timezone?: TimeZone) => {
-  return toZonedTime(Date.now(), timezone?.tzCode ?? "Europe/London");
+export const getNowInTimezone = (timezone?: TimeZone): Date => {
+  return new TZDate(Date.now(), timezone?.tzCode ?? "Europe/London");
 };
 
-export const getDateInTimezone = (date: Date, timezone?: TimeZone) => {
-  return toZonedTime(date, timezone?.tzCode ?? "Europe/London");
-};
-
-export const getStartOfDayGMT = (year: number, month: number, day: number) => {
-  return toZonedTime(new Date(year, month, day), "Europe/London");
+export const getDateInTimezone = (date: Date, timezone?: TimeZone): Date => {
+  return new TZDate(date, timezone?.tzCode ?? "Europe/London");
 };
